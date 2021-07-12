ae3f05928b27925eddfef833b75bd4145409ed0a
raccoonkang <lacoonv@gmail.com>
2021-07-12 11:33:31 +0900

    code push test

--- App.tsx
+++ App.tsx
@@ -64,7 +64,7 @@ const App = () => {
           }}
         >
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then come back to see your edits.
+            Codepush test Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
